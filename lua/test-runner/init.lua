416fcd5382937123dafd94d4830f19ced91a3383
choneface <gavin.garcia0001@gmail.com>
2025-08-12 23:33:38 -0400

    open a terminal and run hello world command

--- lua/test-runner/init.lua
+++ lua/test-runner/init.lua
@@ -1,10 +1,14 @@
 local M = {}
 
-local function open_test_window()
+local function open_test_runner(cmd)
 		local buf = vim.api.nvim_create_buf(false, true)
+		vim.api.nvim_buf_set_option(buf, 'modifiable', false)
+
 		local win = vim.api.nvim_open_win(buf, true, {
 			split = 'right'
 		})
+
+		vim.cmd.terminal(cmd)
 
 		vim.keymap.set('n', 'q', function()
 			vim.api.nvim_win_close(win, false)
@@ -14,7 +18,7 @@ end
 function M.setup()
 	-- TestRunner command
 	vim.api.nvim_create_user_command('TestRunner', function()
-		open_test_window()
+		open_test_runner({"echo \"Hello from the terminal\""})
 	end, {})
 
 	-- Setup the keybinding to run the command
