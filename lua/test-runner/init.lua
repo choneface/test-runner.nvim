61b1efef3c4dc9b2b4d866a53c0628e1cf38f0dd
choneface <gavin.garcia0001@gmail.com>
2025-08-12 22:25:15 -0400

    open side window for test output

--- lua/test-runner/init.lua
+++ lua/test-runner/init.lua
@@ -2,7 +2,14 @@ local M = {}
 
 function M.setup()
 	vim.api.nvim_create_user_command('TestRunner', function()
-		print("Hello from the test function")
+		local buf = vim.api.nvim_create_buf(false, true)
+		local win = vim.api.nvim_open_win(buf, true, {
+			split = 'right'
+		})
+
+		vim.keymap.set('n', 'q', function()
+			vim.api.nvim_win_close(win, false)
+		end, {buffer = buf})
 	end, {})
 end
 
