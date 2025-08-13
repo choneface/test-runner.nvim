bf1b4ecc12a6a517f7f04c1f4645be7623903635
choneface <72355636+choneface@users.noreply.github.com>
2025-08-13 03:19:05 -0700

    Feature: TestRunner Function (#1)
    * define test function
    
    * rename to TestRunner
    
    * open side window for test output
    
    * add keybinding <leader>tr to run the command
    
    * extract opening the window into a function
    
    * open a terminal and run hello world command

---
+++ lua/test-runner/init.lua
@@ -0,0 +1,28 @@
+local M = {}
+
+local function open_test_runner(cmd)
+		local buf = vim.api.nvim_create_buf(false, true)
+		vim.api.nvim_buf_set_option(buf, 'modifiable', false)
+
+		local win = vim.api.nvim_open_win(buf, true, {
+			split = 'right'
+		})
+
+		vim.cmd.terminal(cmd)
+
+		vim.keymap.set('n', 'q', function()
+			vim.api.nvim_win_close(win, false)
+		end, {buffer = buf})
+end
+
+function M.setup()
+	-- TestRunner command
+	vim.api.nvim_create_user_command('TestRunner', function()
+		open_test_runner({"echo \"Hello from the terminal\""})
+	end, {})
+
+	-- Setup the keybinding to run the command
+	vim.keymap.set('n', '<leader>tr', '<cmd>TestRunner<CR>', { noremap = true, silent = true, desc = "Run tests in TestRunner" })
+end
+
+return M
