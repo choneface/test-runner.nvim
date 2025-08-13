ab1da395a3fe6d576d66fab6b9d5ec38451c9346
choneface <gavin.garcia0001@gmail.com>
2025-08-12 22:15:52 -0400

    rename to TestRunner

--- lua/test-runner/init.lua
+++ lua/test-runner/init.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 function M.setup()
-	vim.api.nvim_create_user_command('Test', function()
+	vim.api.nvim_create_user_command('TestRunner', function()
 		print("Hello from the test function")
 	end, {})
 end
