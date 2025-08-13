d12a2bfd09eeab903f3050714db8f985299a2825
choneface <gavin.garcia0001@gmail.com>
2025-08-12 22:34:42 -0400

    add keybinding <leader>tr to run the command

--- lua/test-runner/init.lua
+++ lua/test-runner/init.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 function M.setup()
+	-- TestRunner command
 	vim.api.nvim_create_user_command('TestRunner', function()
 		local buf = vim.api.nvim_create_buf(false, true)
 		local win = vim.api.nvim_open_win(buf, true, {
@@ -11,6 +12,9 @@ function M.setup()
 			vim.api.nvim_win_close(win, false)
 		end, {buffer = buf})
 	end, {})
+
+	-- Setup the keybinding to run the command
+	vim.keymap.set('n', '<leader>tr', '<cmd>TestRunner<CR>', { noremap = true, silent = true, desc = "Run tests in TestRunner" })
 end
 
 return M
